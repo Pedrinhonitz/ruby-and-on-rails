d1ee9d9289b00b885cfc8e2a3e91611f64e4eaf5
Pedrinhonitz <pedrohenriquekleinphg@gmail.com>
2023-01-20 10:24:50 -0300

    :sparkles: feat: Loops Ruby

---
+++ Loops/times.rb
@@ -0,0 +1,11 @@
+2.times do
+    puts "Examples Times in Ruby"
+end
+
+
+times_number = 4
+names = ['Jonh', 'Dean', 'Sammy', 'Scorpian']
+
+times_number.times do |index|
+    puts "Its your name is #{names[index]}"
+end
